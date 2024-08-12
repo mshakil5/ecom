96a920f200a6115de01c5652300760e1c90470ba
Nazmus Sakib <nsakib.miraz@gmail.com>
2024-08-12 18:08:58 +0600

    Mail Content

--- routes/admin.php
+++ routes/admin.php
@@ -30,6 +30,7 @@ use App\Http\Controllers\Admin\BundleProductController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\SubCategoryController;
 use App\Http\Controllers\Admin\PaymentGatewayController;
+use App\Http\Controllers\Admin\MailContentController;
 
 
 Route::group(['prefix' =>'admin/', 'middleware' => ['auth', 'is_admin']], function(){
@@ -295,6 +296,12 @@ Route::group(['prefix' =>'admin/', 'middleware' => ['auth', 'is_admin']], functi
     Route::post('/payment-gateway', [PaymentGatewayController::class, 'store']);
     Route::get('/payment-gateway/{id}/edit', [PaymentGatewayController::class, 'edit']);
     Route::post('/payment-gateway-update', [PaymentGatewayController::class, 'update']);
+
+    // mail content
+    Route::get('/mail-content', [MailContentController::class, 'index'])->name('admin.mail-content');
+    Route::post('/mail-content', [MailContentController::class, 'store']);
+    Route::get('/mail-content/{id}/edit', [MailContentController::class, 'edit']);
+    Route::post('/mail-content-update', [MailContentController::class, 'update']);
 
 });
   
