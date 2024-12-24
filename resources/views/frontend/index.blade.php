3d5542548dab7c65714ab53405fb01fb9a090c59
Nazmus Sakib <nsakib.miraz@gmail.com>
2024-12-24 19:28:47 +0600

    Modal added

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -446,6 +446,180 @@
     <!-- Features End -->
     --}}
 
+    <!-- Start Modal Quickview cart -->
+    <div class="modal fade" id="modalQuickview" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog  modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col text-end">
+                                <button type="button" class="close modal-close" data-bs-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true"> <i class="fa fa-times"></i></span>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="product-details-gallery-area">
+                                    <div class="product-large-image modal-product-image-large">
+                                        <div class="product-image-large-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_1.jpg" alt="">
+                                        </div>
+                                        <div class="product-image-large-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_2.jpg" alt="">
+                                        </div>
+                                        <div class="product-image-large-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_3.jpg" alt="">
+                                        </div>
+                                        <div class="product-image-large-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_4.jpg" alt="">
+                                        </div>
+                                        <div class="product-image-large-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_5.jpg" alt="">
+                                        </div>
+                                        <div class="product-image-large-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_6.jpg" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="product-image-thumb modal-product-image-thumb">
+                                        <div class="zoom-active product-image-thumb-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_1.jpg" alt="">
+                                        </div>
+                                        <div class="product-image-thumb-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_2.jpg" alt="">
+                                        </div>
+                                        <div class="product-image-thumb-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_3.jpg" alt="">
+                                        </div>
+                                        <div class="product-image-thumb-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_4.jpg" alt="">
+                                        </div>
+                                        <div class="product-image-thumb-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_5.jpg" alt="">
+                                        </div>
+                                        <div class="product-image-thumb-single">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_6.jpg" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="product-details-content-area">
+                                    <!-- Start  Product Details Text Area-->
+                                    <div class="product-details-text">
+                                        <h4 class="title">Nonstick Dishwasher PFOA</h4>
+                                        <div class="price"><del>$70.00</del>$80.00</div>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia iste laborum ad impedit pariatur esse optio tempora sint ullam autem deleniti nam in quos qui nemo ipsum numquam, reiciendis maiores quidem aperiam, rerum vel recusandae</p>
+                                    </div> <!-- End  Product Details Text Area-->
+                                    <!-- Start Product Variable Area -->
+                                    <div class="product-details-variable">
+                                        <!-- Product Variable Single Item -->
+                                        <div class="variable-single-item">
+                                            <span>Color</span>
+                                            <div class="product-variable-color">
+                                                <label for="modal-product-color-red">
+                                                    <input name="modal-product-color" id="modal-product-color-red" class="color-select" type="radio" checked>
+                                                    <span class="product-color-red"></span>
+                                                </label>
+                                                <label for="modal-product-color-tomato">
+                                                    <input name="modal-product-color" id="modal-product-color-tomato" class="color-select" type="radio">
+                                                    <span class="product-color-tomato"></span>
+                                                </label>
+                                                <label for="modal-product-color-green">
+                                                    <input name="modal-product-color" id="modal-product-color-green" class="color-select" type="radio">
+                                                    <span class="product-color-green"></span>
+                                                </label>
+                                                <label for="modal-product-color-light-green">
+                                                    <input name="modal-product-color" id="modal-product-color-light-green" class="color-select" type="radio">
+                                                    <span class="product-color-light-green"></span>
+                                                </label>
+                                                <label for="modal-product-color-blue">
+                                                    <input name="modal-product-color" id="modal-product-color-blue" class="color-select" type="radio">
+                                                    <span class="product-color-blue"></span>
+                                                </label>
+                                                <label for="modal-product-color-light-blue">
+                                                    <input name="modal-product-color" id="modal-product-color-light-blue" class="color-select" type="radio">
+                                                    <span class="product-color-light-blue"></span>
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <!-- Product Variable Single Item -->
+                                        <div class="variable-single-item ">
+                                            <span>Quantity</span>
+                                            <div class="product-variable-quantity">
+                                                <input min="1" max="100" value="1" type="number">
+                                            </div>
+                                        </div>
+                                    </div> <!-- End Product Variable Area -->
+                                    <!-- Start  Product Details Meta Area-->
+                                    <div class="product-details-meta mb-20">
+                                        <ul>
+                                            <li><a href=""><i class="icon-heart"></i>Add to wishlist</a></li>
+                                            <li><a href="#" data-bs-toggle="modal" data-bs-target="#modalQuickview"><i class="icon-shopping-cart"></i>Add To Cart</a></li>
+                                        </ul>
+                                    </div> <!-- End  Product Details Meta Area-->
+                                    <!-- Start  Product Details Social Area-->
+                                    <ul class="modal-product-details-social">
+                                        <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#" class="pinterest"><i class="fa fa-pinterest"></i></a></li>
+                                        <li><a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a></li>
+                                        <li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+                                    </ul> <!-- End  Product Details Social Area-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End Modal Quickview cart -->
+
+    <!-- Start Modal Add cart -->
+    <div class="modal fade" id="modalAddcart" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog  modal-dialog-centered modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col text-end">
+                                <button type="button" class="close modal-close" data-bs-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true"> <i class="fa fa-times"></i></span>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-7">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="modal-add-cart-product-img">
+                                            <img class="img-fluid" src="assets/images/products_images/aments_products_image_1.jpg" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="modal-add-cart-info"><i class="fa fa-check-square"></i>Added to cart successfully!</div>
+                                        <div class="modal-add-cart-product-cart-buttons">
+                                            <a href="cart.html">View Cart</a>
+                                            <a href="checkout.html">Checkout</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-5 modal-border">
+                                <ul class="modal-add-cart-product-shipping-info">
+                                    <li> <strong><i class="icon-shopping-cart"></i> There Are 5 Items In Your Cart.</strong></li>
+                                    <li> <strong>TOTAL PRICE: </strong> <span>$187.00</span></li>
+                                    <li class="modal-continue-button"><a href="#" data-bs-dismiss="modal">CONTINUE SHOPPING</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End Modal Add cart -->
+
 @endsection
 
 @section('script')
