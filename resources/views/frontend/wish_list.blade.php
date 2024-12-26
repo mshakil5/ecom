b5e9f1c761335925f5ee887ad3d357713fac4ba7
Nazmus Sakib <nsakib.miraz@gmail.com>
2024-12-26 12:59:24 +0600

    Add wishlist page with product display and actions

--- resources/views/frontend/wish_list.blade.php
+++ resources/views/frontend/wish_list.blade.php
@@ -7,86 +7,99 @@
         $currency = \App\Models\CompanyDetails::value('currency');
     @endphp
 
-    @if($products->isEmpty())
-    <h1 class="title text-center mb-5 mt-4">Wishlist is empty</h1>
-    @else
-    <h1 class="title text-center mb-5 mt-4">Wishlist</h1>
-    <div class="products">
-        <div class="row justify-content-center">
-        @foreach($products as $product)
-            <div class="col-6 col-md-4 col-lg-3">
-                <div class="product product-2">
-                    <figure class="product-media">
-                        <a href="{{ route('product.show', $product->slug) }}">
-                            <x-image-with-loader src="{{ asset('/images/products/' . $product->feature_image) }}" alt="{{ $product->name }}" class="product-image" />
-                        </a>
-
-                        @if ($product->stock && $product->stock->quantity > 0)
-
-                        @php
-                        $colors = $product->stock()
-                            ->where('quantity', '>', 0)
-                            ->distinct('color')
-                            ->pluck('color');
-
-                        $sizes = $product->stock()
-                            ->where('quantity', '>', 0)
-                            ->distinct('size')
-                            ->pluck('size');
-                        @endphp
-
-                            <div class="product-action-vertical">
-                                <a href="#" class="btn-product-icon btn-wishlist add-to-wishlist" title="Add to wishlist" 
-                                data-product-id="{{ $product->id }}"
-                                data-offer-id="{{ $product->offer_id }}"
-                                data-price="{{ $product->offer_price ?? $product->flash_sell_price ?? $product->price }}"
-                                data-campaign-id="{{ $product->campaign_id ?? '' }}"></a>
-                            </div>
-
-                            <div class="product-action">
-                                <a href="#" class="btn-product btn-cart" title="Add to cart"
-                                data-product-id="{{ $product->id }}" 
-                                data-offer-id="{{ $product->offer_id }}" 
-                                data-price="{{ $product->price }}" 
-                                data-toggle="modal" data-target="#quickAddToCartModal" 
-                                data-image ="{{ asset('images/products/' . $product->feature_image) }}" data-stock="{{ $product->stock->quantity }}"
-                                data-colors="{{ $colors->toJson() }}"
-                                data-sizes="{{ $sizes->toJson() }}">
-                                    <span>add to cart</span>
-                                </a>
-                            </div>
-                        @else
-                            <span class="product-label label-out-stock">Out of Stock</span>
-                        @endif
-                    </figure>
-
-                    <div class="product-body">
-                        <h3 class="product-title"><a href="{{ route('product.show', $product->slug) }}">{{ $product->name }}</a></h3>
-                        <div class="product-price">
-                            @if(isset($product->offer_price))
-                                <del>{{ $currency }} {{ number_format($product->price, 2) }}</del>
-                                {{ $currency }} {{ number_format($product->offer_price, 2) }}
-                                @php
-                                    $discountPercentage = (($product->price - $product->offer_price) / $product->price) * 100;
-                                @endphp
-                                <small>({{ round($discountPercentage, 0) }}% off)</small>
-                            @elseif(isset($product->flash_sell_price))
-                                <del>{{ $currency }} {{ number_format($product->price, 2) }}</del>
-                                {{ $currency }} {{ number_format($product->flash_sell_price, 2) }}
-                                @php
-                                    $discountPercentage = (($product->price - $product->flash_sell_price) / $product->price) * 100;
-                                @endphp
-                                <small>({{ round($discountPercentage, 0) }}% off)</small>
-                            @else
-                                {{ $currency }} {{ number_format($product->price, 2) }}
-                            @endif
+    <div class="breadcrumb-section">
+        <div class="breadcrumb-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-between justify-content-md-between  align-items-center flex-md-row flex-column">
+                        <h3 class="breadcrumb-title">Wishlist</h3>
+                        <div class="breadcrumb-nav">
+                            <nav aria-label="breadcrumb">
+                                <ul>
+                                    <li><a href="{{ route('frontend.homepage') }}">Home</a></li>
+                                    <li class="active" aria-current="page">Wishlist</li>
+                                </ul>
+                            </nav>
                         </div>
                     </div>
                 </div>
             </div>
-        @endforeach
         </div>
     </div>
+
+    @if($products->isEmpty())
+    <h1 class="title text-center mb-5 mt-4">Wishlist is empty</h1>
+    @else
+        <h1 class="title text-center mb-5 mt-4">Wishlist</h1>
+        <div class="wishlist-section">
+            <div class="wishlish-table-wrapper" data-aos="fade-up" data-aos-delay="0">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="table_desc">
+                                <div class="table_page table-responsive">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th class="product_remove">Delete</th>
+                                                <th class="product_thumb">Image</th>
+                                                <th class="product_name">Product</th>
+                                                <th class="product-price">Price</th>
+                                                <th class="product_stock">Stock Status</th>
+                                                <th class="product_addcart">Add To Cart</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach($products as $product)
+                                                <tr>
+                                                    <td class="product_remove">
+                                                        <a href="#" class="remove-from-wishlist" data-product-id="{{ $product->id }}">
+                                                            <i class="fa fa-trash-o"></i>
+                                                        </a>
+                                                    </td>
+                                                    <td class="product_thumb">
+                                                        <a href="{{ route('product.show', $product->slug) }}">
+                                                            <img src="{{ asset('/images/products/' . $product->feature_image) }}" alt="{{ $product->name }}">
+                                                        </a>
+                                                    </td>
+                                                    <td class="product_name">
+                                                        <a href="{{ route('product.show', $product->slug) }}">{{ $product->name }}</a>
+                                                    </td>
+                                                    <td class="product-price">
+                                                        @if(isset($product->offer_price))
+                                                            <del class="product-default-price-off">{{ $currency }} {{ number_format($product->price, 2) }}</del>
+                                                            {{ $currency }} {{ number_format($product->offer_price, 2) }}
+                                                        @elseif(isset($product->flash_sell_price))
+                                                            <del class="product-default-price-off">{{ $currency }} {{ number_format($product->price, 2) }}</del>
+                                                            {{ $currency }} {{ number_format($product->flash_sell_price, 2) }}
+                                                        @else
+                                                            {{ $currency }} {{ number_format($product->price, 2) }}
+                                                        @endif
+                                                    </td>
+                                                    <td class="product_stock">
+                                                        {{ $product->stock_status ?? 'In Stock' }}
+                                                    </td>
+                                                    <td class="product_addcart">
+                                                        <a href="#" class="btn-product btn-cart" 
+                                                        data-product-id="{{ $product->id }}" 
+                                                        data-offer-id="{{ $product->offer_id }}" 
+                                                        data-price="{{ $product->price }}" 
+                                                        data-toggle="modal" data-target=".quickAddToCartModal" 
+                                                        data-image="{{ asset('images/products/' . $product->feature_image) }}">
+                                                            Add To Cart
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     @endif
 
 </div>
